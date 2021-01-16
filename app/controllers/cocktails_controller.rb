9a3aa0e8c8c0d897635c202484f77c9191221370
Joaquin Casanova <joaco.c91@gmail.com>
2021-01-16 13:39:26 -0300

    added CRUD

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,33 @@
+class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show]
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @dose = Dose.new
+  end
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+ingredient_1 = Ingredient.create(name: "lemon")
+ingredient_2 = Ingredient.create(name: "ice")
+ingredient_3 = Ingredient.create(name: "mint leaves")
+
+cocktail_1 = Cocktail.create(name: "Whisky on the rocks")
+cocktail_2 = Cocktail.create(name: "Mojito")
+cocktail_3 = Cocktail.create(name: "Sex on the beach")
+
+Dose.create(description:"1Kg", cocktail: cocktail_1, ingredient: ingredient_1 )
+Dose.create(description:"1Lt", cocktail: cocktail_2, ingredient: ingredient_2 )
+Dose.create(description:"3Mg", cocktail: cocktail_3, ingredient: ingredient_3)
